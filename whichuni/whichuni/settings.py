d40c7cd501832b51d37b1477f3eff0f63bced4f5
plaza <plazauk@hotmail.co.uk>
2016-08-23 18:35:59 +0100

    turn off debug mode

--- whichuni/whichuni/settings.py
+++ whichuni/whichuni/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!90i^j#hlh0v9c12+&&e%1o!vdg_!thprwu2hdft0@n1yyi7s5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
